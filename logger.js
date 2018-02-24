e4126c549eead2a34bb2f8b5d2c8d125872b1772
Davenson Lombard <lombard.davenson@gmail.com>
2018-02-24 16:42:23 -0500

    messageid in logger

--- logger.js
+++ logger.js
@@ -2,6 +2,7 @@ let winston = require('winston');
 let moment = require('moment')
 const config = winston.config;
 const tsFormat = () => moment().format();
+let loggers = {}
 
 let logger = new winston.Logger({
   transports: [
@@ -17,42 +18,44 @@ let logger = new winston.Logger({
 
 const createLogger = (messageId) => {
   if (!messageId) {
-    return new winston.Logger({
-      transports: [
-        new (winston.transports.Console)({
-          timestamp: tsFormat,
-          colorize: true,
-          handleExceptions: true,
-          json: false,
-        })
-      ],
-      exitOnError: false,
-    });
-  }
-  else {
-    let logger = new winston.Logger({
-      transports: [
-        new (winston.transports.Console)({
-          timestamp: tsFormat,
-          colorize: true,
-          handleExceptions: true,
-          json: false,
-          formatter: (options) => {
-            return options.timestamp() + ' ' +
-              config.colorize(options.level, options.level.toUpperCase()) + ' ' + `[${messageId}] - ` +
-              (options.message ? options.message : '') +
-              (options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '');
-          }
-        })
-      ],
-      exitOnError: false,
-    });
     return logger
   }
+  else {
+    if (!loggers[messageId]) {
+      let mylogger = new winston.Logger({
+        transports: [
+          new (winston.transports.Console)({
+            timestamp: tsFormat,
+            colorize: true,
+            handleExceptions: true,
+            json: false,
+            formatter: (options) => {
+              return options.timestamp() + ' - ' +
+                config.colorize(options.level, `${options.level.toLowerCase()}`) + ': ' + `[${messageId}] - ` +
+                (options.message ? options.message : '') +
+                (options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '');
+            }
+          })
+        ],
+        exitOnError: false,
+
+      });
+      loggers[messageId] = mylogger
+      return mylogger
+    }
+    return loggers[messageId]
+  }
 }
-module.exports = createLogger;
+
+const deleteLogger = (messageId) => {
+  if (loggers[messageId]) {
+    delete loggers[messageId]
+  }
+}
+module.exports.delete = deleteLogger
+module.exports.logger = createLogger;
 module.exports.stream = {
   write: function (message, encoding) {
-    _logger.info(message);
+    logger.info(message);
   },
 };
